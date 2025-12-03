11e11c5aea98949143c4c8684f1078d925286c51
Wisnu Adigama <adigama33@gmail.com>
2025-12-03 12:55:21 +0800

    Membuat form add produk supaya admin bisa menambahkan produk

--- create.php
+++ create.php
@@ -1,3 +1,7 @@
+<?php
+require_once 'inc/config.php';
+Utility::checkLogin();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,33 +16,52 @@
     </header>
     <main>
         <section>
-            <form action="saveProduct.php" method="post" id="form-product">
-                <div class="row"></div>
+            <form action="saveProduct.php" method="post" enctype="multipart/form-data" id="form-products">
                 <div class="row">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" value="" required>
+                    <label for="name">Nama Produk:</label>
+                    <input type="text" id="name" name="name" required placeholder="Contoh: Beras 5kg">
                 </div>
+
                 <div class="row">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
+                    <label for="category">Kategori:</label>
+                    <select id="category" name="category" required>
+                        <option value="" disabled selected>Pilih Kategori</option>
+                        <option value="Sembako">Makanan</option>
+                        <option value="Minuman">Minuman</option>
+                        <option value="Makanan Ringan">Makanan Ringan</option>
+                        <option value="Rokok">Rokok</option>
+                        <option value="Perlengkapan Mandi">Perlengkapan Mandi</option>
+                        <option value="Lain-lain">Lain-lain</option>
+                    </select>
                 </div>
+
                 <div class="row">
-                <label for="confirm">Confirm:</label>
-                <input type="password" id="confirm" name="confirm" required>
+                    <label for="price">Harga (Rp):</label>
+                    <input type="number" id="price" name="price" required min="0" placeholder="Masukkan Harga">
                 </div>
+
+                <div class="row">
+                    <label for="stock">Stok Awal:</label>
+                    <input type="number" id="stock" name="stock" required min="0" placeholder="Masukkan stok satuannya">
+                </div>
+
+                <div class="row">
+                    <label for="image">Foto Produk:</label>
+                    <input type="file" id="image" name="image" accept="image/*" required>
+                    <br><small style="margin-left: 125px; color: grey;">Format: JPG, PNG, GIF</small>
+                </div>
+
+                <div class="row">
+                    <label for="status">Status:</label>
+                    <select id="status" name="status">
+                        <option value="active">Active (Dijual)</option>
+                        <option value="inactive">Inactive (Disimpan)</option>
+                    </select>
+                </div>
+
                 <hr>
                 <div class="row">
-                <label for="fullname">Full Name:</label> 
-                <input type="text" id="fullname" name="fullname" value="" required>
-                </div>
-                <div class="row">
-                <label for="city">City:</label>
-                <input type="text" id="city" name="city" value="" required>
-                </div>
-                <hr>
-                <div class="row"><?php Utility::showFlash(); ?></div>
-                <div class="row">
-                <button type="submit">Daftar</button>
+                    <button type="submit">Save Product</button>
                 </div>
             </form>
         </section>
